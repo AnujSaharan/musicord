cad61673a3250a5fc0801fb7f5804f21a09a0446
Anuj Saharan <anujsinghsaharan@gmail.com>
2018-10-20 03:11:10 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,95 @@
+$(function () {
+
+    var socket = io();
+
+    socket.on('time', function (msg) {
+        // $('.timer-div').html(msg);
+        // $('.timer-div').html(audio.currentTime);
+    });
+
+    var audio = document.getElementById('audio-id');
+
+    
+
+    socket.on('first', function (msg) {
+        // $('#audio-id').attr('controls', "1"); 
+
+        // $('.control-button').show();
+        // $('.control-button').removeClass('hide');
+
+        audio.ontimeupdate = function () {
+            socket.emit("seek", audio.currentTime);
+        // $('.timer-div').html(audio.currentTime);
+        };
+
+        $('#play-button').click(function() {
+            console.log("Play Clicked");
+            socket.emit("play", audio.currentTime);
+            
+        });
+
+        $('#pause-button').click(function() {
+            console.log("Pause Clicked");
+            socket.emit("pause", audio.currentTime);
+        });
+
+        $('input[name=track]').attr('disabled', false);
+
+        $('input[name=track]').click(function() {
+             socket.emit("reset", $(this).val());
+        });
+    });
+
+    socket.on("current", function (msg) {
+        var diff = audio.currentTime - msg;
+        if (diff < 0 || diff > 2) {
+            audio.currentTime = msg;
+        }
+        $('.timer-span').html(msg);
+    });
+
+    socket.on("reset", function (msg) {
+        console.log(msg);
+        socket.emit("pause", 0.00);
+        $('input[name=track]').attr('checked', false);        
+        $('input[value='+ msg +']').attr('checked', true);
+
+        var label = "";
+
+        if (msg == 1) {
+            label = "Na Na Na (Na Na Na Na Na Na Na Na Na)";
+            $('.audio-source').attr('src', 'music/Na Na Na (Na Na Na Na Na Na Na Na Na).mp3')
+        }
+        else if (msg == 2) {
+            label = "Planetary (GO!)";
+            $('.audio-source').attr('src', 'Planetary (GO).mp3')
+        }
+        if (msg == 3) {
+            label = "Famous Last Words";
+            $('.audio-source').attr('src', 'music/Famous Last Words.mp3')
+        }
+
+        $('.label').html(label);
+        audio.load();
+    });
+
+
+
+    socket.on("play", function (msg) {
+        audio.currentTime = msg;
+        audio.play();
+        $('#play-button').addClass('hide');
+        $('#pause-button').removeClass('hide');
+    });
+
+    socket.on("pause", function(msg) {
+        audio.currentTime = msg;
+        audio.pause();
+        $('#play-button').removeClass('hide');
+        $('#pause-button').addClass('hide');
+    })
+
+
+
+
+});
